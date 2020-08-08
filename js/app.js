24c0973262018eca0cf53892ec1c964781cde6a4
Kamil Kamil <kdbrasi360@gmail.com>
2020-08-08 13:50:54 +0200

    added control with keyboardarrows and collision with coin event

--- js/app.js
+++ js/app.js
@@ -25,7 +25,7 @@ class Game {
   }
   
   index(x, y) {
-    return x + (y * 10);
+    return x + (10 * y);
   }
   
   showFurry() {
@@ -35,8 +35,9 @@ class Game {
   showCoin() {
     this.board[this.index(this.coin.x, this.coin.y)].classList.add('coin')
   }
-  
-
+  hideCoin(){
+    this.board[this.index(this.coin.x, this.coin.y)].classList.remove('coin')
+  }
   
   moveFurry() {
     if (this.furry.direction === 'right') {
@@ -44,26 +45,59 @@ class Game {
     } else if (this.furry.direction === 'left') {
       this.furry.x = this.furry.x - 1;
     } else if (this.furry.direction === 'up') {
-      this.furry.x = this.furry.y + 1;
+      this.furry.y = this.furry.y - 1;
     } else if (this.furry.direction === 'down') {
-      this.furry.x = this.furry.y - 1;
+      this.furry.y = this.furry.y + 1;
     }
     this.hideVisibleFurry()
     this.showFurry();
+    this.checkCoinCollision();
   }
   
   startGame() {
-    const self= this;
-    this.idSetInterval = setInterval(function (){
+    const self = this;
+    this.idSetInterval = setInterval(function () {
       self.moveFurry();
     }, 250)
   }
-  hideVisibleFurry(){
-   const furryClass= document.querySelector('.furry');
-   furryClass.classList.remove('furry');
-}
+  
+  hideVisibleFurry() {
+    const furryClass = document.querySelector('.furry');
+    furryClass.classList.remove('furry');
+  }
+  
+  turnFurry(turnEvent = 38) {
+    switch (turnEvent.which) {
+      case 37:
+        this.furry.direction = 'left'
+        break;
+      case 39:
+        this.furry.direction = 'right'
+        break;
+      case 38:
+        this.furry.direction = 'up'
+        break;
+      case 40:
+        this.furry.direction = 'down'
+        break;
+    }
+  }
+  
+  checkCoinCollision(){
+    if (this.index(this.furry.x, this.furry.y) === this.index(this.coin.x, this.coin.y)){
+      this.hideCoin();
+     this.score ++;
+      const scoreBoard=document.getElementById('score');
+      scoreBoard.innerText= this.score;
+      this.coin = new Coin;
+      this.showCoin();
+    }
+  }
 }
 
+document.addEventListener('keydown', function (event) {
+  game.turnFurry(event);
+});
 const game = new Game;
 game.showCoin();
 game.showFurry();
