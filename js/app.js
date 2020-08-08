4d1a13ad96563db6bdf5ce4efbc419f94505395b
Kamil Kamil <kdbrasi360@gmail.com>
2020-08-08 12:21:01 +0200

    furry can move now!

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ import "../sass/main.scss";
 
 
 class Furry {
-  constructor(x=0, y=0, direction='right') {
+  constructor(x = 0, y = 0, direction = 'right') {
     this.x = x
     this.y = y
     this.direction = direction
@@ -20,25 +20,51 @@ class Game {
   constructor() {
     this.board = document.querySelectorAll('#board div')
     this.furry = new Furry()
-    this.coin= new Coin()
-    this.score= 0
+    this.coin = new Coin()
+    this.score = 0
   }
-  index(x,y) {
-    return x + (y*10);
+  
+  index(x, y) {
+    return x + (y * 10);
   }
-  showFurry(){
+  
+  showFurry() {
     this.board[this.index(this.furry.x, this.furry.y)].classList.add('furry')
   }
-  showCoin(){
+  
+  showCoin() {
     this.board[this.index(this.coin.x, this.coin.y)].classList.add('coin')
   }
-  startGame(){
-    this.idSetInterval= setInterval(function (){
-      console.log('text z intreval');
+  
+
+  
+  moveFurry() {
+    if (this.furry.direction === 'right') {
+      this.furry.x = this.furry.x + 1;
+    } else if (this.furry.direction === 'left') {
+      this.furry.x = this.furry.x - 1;
+    } else if (this.furry.direction === 'up') {
+      this.furry.x = this.furry.y + 1;
+    } else if (this.furry.direction === 'down') {
+      this.furry.x = this.furry.y - 1;
+    }
+    this.hideVisibleFurry()
+    this.showFurry();
+  }
+  
+  startGame() {
+    const self= this;
+    this.idSetInterval = setInterval(function (){
+      self.moveFurry();
     }, 250)
   }
+  hideVisibleFurry(){
+   const furryClass= document.querySelector('.furry');
+   furryClass.classList.remove('furry');
 }
-const game= new Game;
+}
+
+const game = new Game;
 game.showCoin();
 game.showFurry();
 game.startGame();
